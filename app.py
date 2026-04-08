70aa8c1d95f4bf0e447163b5e74161fa01ce505f
data_holick <111300107+DATA-Meta@users.noreply.github.com>
2026-04-08 03:05:12 +0200

    Varible rule for failure

--- app.py
+++ app.py
@@ -15,7 +15,18 @@ def welcome():
 def index():
     return "<h2>Welcome to the Index page</h2>"
 
+#Variable rule to add values, with type casting defining data types
 
+@app.route("/success/<int:score>")
+def success(score):
+    return "<h3>The Person has passed and the score is: </h3>"+ str(score)
+
+
+#Varible rule for failure
+
+app.route("/fail/<int:score>")
+def success(score):
+    return "<h4>The Person has failed and the score is: </h4>"+ str(score)
 
 if __name__ == '__main__':
     app.run(debug=True)
